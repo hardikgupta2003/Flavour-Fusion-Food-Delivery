561c051f20019d76967a88c40e558b19ced0a984
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-06-15 10:29:29 +0530

    Added for hosting purpose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose"
 
 export const DBConnect = async () => {
-    await mongoose.connect("mongodb+srv://hardikgupta:gQLqOgNR26ZHbqIF@cluster0.a1ttvze.mongodb.net/Food-DelDB")
-    .then(()=>{
-        console.log('DB connected successfully')
-    })
-    .catch((e)=>{
-        console.log("DB connection Failed : ",e)
-    })
+    await mongoose
+      .connect(process.env.MONGODB_URL)
+      .then(() => {
+        console.log("DB connected successfully");
+      })
+      .catch((e) => {
+        console.log("DB connection Failed : ", e);
+      });
 }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { configDotenv } from "dotenv";
-import 'dotenv/config'
+import "dotenv/config";
 import { DBConnect } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
@@ -10,13 +10,11 @@ import orderRouter from "./routes/orderRoutes.js";
 
 // app config
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 4000;
 
 //middlewares
 app.use(express.json());
-app.use(
-  cors()
-);
+app.use(cors());
 
 //Db connection
 DBConnect();
@@ -24,9 +22,9 @@ DBConnect();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter )
-app.use("/api/order",orderRouter )
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API is running successfully");
